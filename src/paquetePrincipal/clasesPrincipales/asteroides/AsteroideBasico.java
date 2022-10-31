d5be4e77345eeac127467bb7e238ed8e0b2ce025
Manu Prudencio <manu.prudencio@opendeusto.es>
2022-10-31 19:34:45 +0100

    Paquete Asteroides con Clases

---
+++ src/paquetePrincipal/clasesPrincipales/asteroides/AsteroideBasico.java
@@ -0,0 +1,12 @@
+package paquetePrincipal.clasesPrincipales.asteroides;
+
+public class AsteroideBasico extends Asteroide {
+	protected static String IMG = "";
+	
+	public AsteroideBasico() {
+		// TODO Auto-generated constructor stub
+		super(1, 1, 2, 2, 50, 50, IMG);
+		
+	}
+
+}
---
+++ src/paquetePrincipal/clasesPrincipales/asteroides/AsteroideDorado.java
@@ -0,0 +1,13 @@
+package paquetePrincipal.clasesPrincipales.asteroides;
+
+public class AsteroideDorado extends Asteroide {
+	protected static String IMG = "";
+
+	public AsteroideDorado() {
+	// TODO Auto-generated constructor stub
+		super(3, 1, 2, 2, 50, 50, IMG);
+			
+		
+
+}
+}
