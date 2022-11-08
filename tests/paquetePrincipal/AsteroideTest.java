4ac504c3e266cc894feb3967dc6e7365b236af90
Manu Prudencio <manu.prudencio@opendeusto.es>
2022-11-08 20:40:13 +0100

    Añado Tests de Asteroide

---
+++ tests/paquetePrincipal/AsteroideTest.java
@@ -0,0 +1,26 @@
+package paquetePrincipal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import paquetePrincipal.clasesPrincipales.asteroides.AsteroideBasico;
+import paquetePrincipal.clasesPrincipales.asteroides.AsteroideDorado;
+import paquetePrincipal.clasesPrincipales.enemigos.EnemigoBasico;
+
+public class AsteroideTest {
+
+	@Test
+	public void testSetDibujoInicial() {
+		AsteroideBasico ab = new AsteroideBasico();
+		ab.cicloAnimacion.add("imagen0");
+		ab.cicloAnimacion.add("imagen1");
+		ab.cicloAnimacion.add("imagen2");
+		
+		AsteroideDorado ad = new AsteroideDorado();
+		ad.cicloAnimacion.add("imagen0");
+		ad.cicloAnimacion.add("imagen1");
+		ad.cicloAnimacion.add("imagen2");
+	}
+
+}
